0a6e238e2f9b8f4618963bd9c1a37c16cf8936c6
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-06-10 16:20:52 +0200

    Send message Controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,13 +1,50 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
-export const send = (req, res) => {};
+export const send = async (req, res) => {
+  try {
+    const {id: receiverId} = req.params;
+    const senderId = req.user._id;
+    const {text, image} = req.body;
+    if (!text && !image)
+      return res
+        .status(400)
+        .json({message: "Le message doit avoir un contenu."});
+
+    if (!(await User.findById(receiverId)))
+      return res.status(400).json({message: "Utilisateur inconnu."});
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      image: imageUrl,
+      text,
+    });
+
+    if (!newMessage)
+      return res.status(400).json({message: "Contenu de message invalide."});
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Send Message error :", error);
+    res.status(500);
+  }
+};
 
 export const getUsersForSidebar = async (req, res) => {
   try {
     const LoggedId = req.user._id;
     const filteredUsers = await User.find({_id: {$ne: LoggedId}}).select(
-      "-password"
+      "-password -email -createdAt -updatedAt -__v"
     );
     res.status(200).json(filteredUsers);
   } catch (error) {
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -3,10 +3,12 @@ import {protectRoute} from "../middlewares/auth.middleware.js";
 import {
   getMessages,
   getUsersForSidebar,
+  send,
 } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, send);
 
 export default router;
