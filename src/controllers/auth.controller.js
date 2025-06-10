9183472306cfbce4a3b1a5321af4d4491239d1dc
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-06-10 14:41:17 +0200

    Signin Controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,10 +5,11 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const {fullname, email, password} = req.body;
   try {
-    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{5,}$/.test(password)) {
+    if (password.length < 5) {
       res.status(400).json({
         message: "Le mot de passe doit correspondre aux normes actuelles.",
       });
+      return;
     }
 
     const user = await User.findOne({email});
@@ -29,7 +30,7 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      const token = generateToken(newUser._id, res);
+      generateToken(newUser._id, res);
       await newUser.save();
       res.status(201).json({
         _id: newUser._id,
@@ -49,7 +50,25 @@ export const signup = async (req, res) => {
 };
 
 export const signin = async (req, res) => {
+  const {email, password} = req.body;
   try {
+    const user = await User.findOne({email});
+    if (!user) {
+      res.status(400).json({message: "Mauvais identifiants."});
+      return;
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.status(400).json({message: "Mauvais identifiants."});
+      return;
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      avatar: user.avatar,
+    });
   } catch (error) {
     console.log("Signin error :", error);
     res.status(500);
