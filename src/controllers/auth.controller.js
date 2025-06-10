7c18da89030ca169cc24481aaced798b5b98d6ff
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-06-10 13:54:04 +0200

    controller init

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+export const signup = (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export const signin = (req, res) => {};
+
+export const signout = (req, res) => {};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import {connectionDB} from "./lib/db.js";
 
+dotenv.config();
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running.");
-  console.log("\x1b[36m%s\x1b[0m", `http://localhost:5000/api/`);
+  console.log("\x1b[36m%s\x1b[0m", `http://localhost:${process.env.PORT}/api/`);
+  connectionDB();
 });
